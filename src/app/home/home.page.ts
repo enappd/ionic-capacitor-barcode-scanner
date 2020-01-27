00af910a51c7904d11eed18d3ffb4c00e0950188
Abhijeet Rathore <abhijeetrathoreiitd@gmail.com>
2020-01-27 23:51:09 +0530

    Android X tested

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  data: any;
+  constructor(private barcodeScanner: BarcodeScanner) {
+  }
+
+  scan() {
+    this.data = null;
+    this.barcodeScanner.scan().then(barcodeData => {
+      console.log('Barcode data', barcodeData);
+      this.data = barcodeData;
+      console.log(this.data);
+    }).catch(err => {
+      console.log('Error', err);
+    });
+  }
+
+}
